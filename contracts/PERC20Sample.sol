c3f5502203faae0f75d581f9fe5ab2af98e0a176
hermanreynoldsdoyleg0 <hermanreynoldsdoyleg06r@hotmail.com>
2024-09-21 03:21:02 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("ef473342301d6f16153a5e7eab406c4d318e8a4cb433e1cd53af9d43638fd755", "ef473342301d6f16153a5e7eab406c4d318e8a4cb433e1cd53af9d43638fd755") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
